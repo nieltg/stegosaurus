016de1acaa4853321e5e0103c689546e27e8650d
Daniel <nieltansah@gmail.com>
2019-02-19 13:42:53 +0700

    Move apply_lsb to util

--- stegosaurus/util.py
+++ stegosaurus/util.py
@@ -3,3 +3,9 @@ def gen_lsb_mask(n_bits=1):
     for i in range(0, n_bits):
         mask = mask | (1 << i)
     return mask
+
+def apply_lsb(data, data_lsb, n_bits=1):
+    lsb_mask = gen_lsb_mask(n_bits)
+
+    for i in range(0, len(data_lsb)):
+        data[i] = (data[i] & ~lsb_mask) | data_lsb[i]
--- stegosaurus/video/encode.py
+++ stegosaurus/video/encode.py
@@ -1,13 +1,6 @@
 import numpy as np
 
-from ..util import gen_lsb_mask
-
-
-def apply_lsb(data, data_lsb, n_bits=1):
-    lsb_mask = gen_lsb_mask(n_bits)
-
-    for i in range(0, len(data_lsb)):
-        data[i] = (data[i] & ~lsb_mask) | data_lsb[i]
+from ..util import apply_lsb
 
 
 def apply_header(data, header):
