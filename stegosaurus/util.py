22731cc5cb17e3d4861976d2ca97728485a4d6d4
Daniel <nieltansah@gmail.com>
2019-02-19 13:16:34 +0700

    util: Remove unused incomplete method

--- stegosaurus/util.py
+++ stegosaurus/util.py
@@ -1,12 +1,5 @@
-from numpy.random import RandomState
-
-
 def gen_lsb_mask(n_bits=1):
     mask = 0
     for i in range(0, n_bits):
         mask = mask | (1 << i)
     return mask
-
-def random_range(start, stop):
-    np.Rando
-    range(start, stop)
